75079cc4ae7c54959a7da26c0351b017a3080b6a
Itamar Shaked <Itamar@shaked.in>
2026-05-27 22:25:04 +0300

    Use environment variables

--- database.py
+++ database.py
@@ -1,10 +1,26 @@
+import os
+import time
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://petuser:petpass@localhost:5432/petsdb"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://petuser:petpass@localhost:5432/petsdb"
+)
 
-engine = create_engine(DATABASE_URL)
+# Retry connection
+for i in range(10):
+    try:
+        engine = create_engine(DATABASE_URL)
+        connection = engine.connect()
+        connection.close()
+        print("Database connected!")
+        break
+    except Exception as e:
+        print("Database not ready yet...")
+        time.sleep(2)
 
 SessionLocal = sessionmaker(
     autocommit=False,
