3f0f7a1a27b902017da8c4d0e26bd5f11f0bc510
Itamar Shaked <Itamar@shaked.in>
2026-05-27 22:08:23 +0300

    Use Postgres database

--- database.py
+++ database.py
@@ -1,13 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./pets.db"
+DATABASE_URL = "postgresql://petuser:petpass@localhost:5432/petsdb"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
